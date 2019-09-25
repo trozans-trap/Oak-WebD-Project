7d1b2af8c5a2f9030520f564741f8d0c14802cf4
trozans-trap <chawlavikalp2000@gmail.com>
2019-09-25 15:45:37 +0530

    some add ons

--- oak.js
+++ oak.js
@@ -1,21 +1,4 @@
 
-// var index = 0 ;
-// imageSlider();
-
-// function imageSlider() {
-// 	var i;
-// 	var x = document.querySelector(".slider");
-//     for( i=0;i<x.length;i++){
-//     	x[i].style.display="none";
-//     }
-//     index++;
-//     if( index > x.length){
-//     	index=1
-//     }
-//     x[index-1].style.display ="block";
-//     setTimeout(imageSlider,2000);
-// }
-
 
 var myIndex = 0;
 carousel();
@@ -29,7 +12,7 @@ function carousel() {
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
   x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 4000); // Change image every 2 seconds
+  setTimeout(carousel, 4000); 
 }
 
 
@@ -45,5 +28,39 @@ function newcarousel() {
   newmyIndex++;
   if (newmyIndex > y.length) {newmyIndex = 1}    
   y[newmyIndex-1].style.display = "block";  
-  setTimeout(newcarousel, 3000); // Change image every 2 seconds
+  setTimeout(newcarousel, 3000); 
+}
+
+
+// sidenavbar
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "420px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
